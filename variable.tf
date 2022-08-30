6737077e9f472cd0269e6dd985a36147eafa8c0a
Israel Jack <israel.jack90@gmail.com>
2022-08-30 19:51:24 -0400

    created route53 file

--- variable.tf
+++ variable.tf
@@ -112,3 +112,16 @@ variable "ec2_key_pair_name" {
     description     = "name of the ec2 key"
     type            = string
 }
+
+# route55 variables
+variable "domain_name" {
+    default         = "zzdynasty.com"
+    description     = "domain name"
+    type            = string
+}
+
+variable "record_name" {
+    default         = "www"
+    description     = "sub domain name"
+    type            = string
+}
